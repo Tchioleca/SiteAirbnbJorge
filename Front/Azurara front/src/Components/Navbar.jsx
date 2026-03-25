fe928c524c80b4aa0fcacd7fae85adeae5d9c638
VoidRafouz <73494222+VoidRafouz@users.noreply.github.com>
2026-03-25 21:37:50 +0000

    created pages

---
+++ Front/Azurara front/src/Components/Navbar.jsx
@@ -0,0 +1,30 @@
+import {Link, useLocation } from "react-router-dom";
+import {useContext, useState } from "react";
+
+
+/*Todo 
+left to right :
+Lodging name,
+search bar(check-in dates , checkout dates , number of guests as a dropdown, call to action button (SEARCH) => mount info in temporary component then serve it directly in the CheckAvailability component = Then => serve all the info in the Booking page )
+!! presistent component in all pages 
+{
+links to : 
+/Rooms & Suites ( /pages/RoomsPage);    
+/About ( /pages/AboutPage);
+/Contacts (/pages/ContactPage);
+
+
+}
+
+const NAV_LINKS = [
+{label:"Home", to: "/"},
+{label:"About", to: "/about"}
+{label:"Contacts", to: "/"}
+];
+
+*/
+
+function Navbar (){
+    const location = useLocation();
+    
+}
